dc097d4bd4fde9f2de163df71f48e9360f5e4841
Ramir Junior <junior.ribeiro0@live.com>
2023-11-23 19:30:50 -0300

    Configuring JPA and Repository

--- src/main/java/br/com/ramir/springjobmanager/candidate/controller/CandidateController.java
+++ src/main/java/br/com/ramir/springjobmanager/candidate/controller/CandidateController.java
@@ -1,20 +1,24 @@
 package br.com.ramir.springjobmanager.candidate.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.ramir.springjobmanager.candidate.Candidate;
+import br.com.ramir.springjobmanager.candidate.CandidateRepository;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/candidate")
 public class CandidateController {
+
+    @Autowired
+    private CandidateRepository candidateRepository;
     
     @PostMapping("/")
-    public void create(@Valid @RequestBody Candidate candidate){
-        System.out.println("Candidato");
-        System.out.println(candidate.getEmail());
+    public Candidate create(@Valid @RequestBody Candidate candidate){
+        return this.candidateRepository.save(candidate);
     }
 }
